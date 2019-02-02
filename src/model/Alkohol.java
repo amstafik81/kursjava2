b4aca5030f3aeb36beb325508433d606f3f08f58
Tomasz O <amstafik81@gmail.com>
2019-02-02 12:13:51 +0100

    dodano sklep

---
+++ src/model/Alkohol.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Alkohol {
+    private String nazwa;
+    private BigDecimal cena;
+    private String litraz;
+    private String procent;
+    private int id;
+    private static int generator=1;
+    //konstruktor
+   public Alkohol(String nazwa,BigDecimal cena,String litraz,String procent){
+        this.nazwa=nazwa;
+        this.cena=cena;
+        this.litraz=litraz;
+        this.procent=procent;
+        this.id=generator++;
+    }
+    public int getId(){
+       return id;
+    }
+}
---
+++ src/model/Sklep.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Sklep {
+
+
+    private String nazwa;
+    private List<Alkohol> alkohole;
+    public Sklep(String nazwa) {
+        this.nazwa = nazwa;
+        this.alkohole=new ArrayList<>();
+    }
+
+   public void dodajAlkohol(Alkohol alkohol){
+        alkohole.add(alkohol);
+    }
+    public void usunAlkohol(int id){
+        Iterator<Alkohol> iterator=alkohole.iterator();
+        while (iterator.hasNext()){
+            Alkohol alkohol=iterator.next();
+            if(alkohol.getId()==id){
+                iterator.remove();
+            }
+
+        }
+    }
+    public void wyswietlAlkohol(){
+        System.out.println(alkohole);
+        for(Alkohol i:alkohole){
+            System.out.println(i);
+        }
+    }
+}
